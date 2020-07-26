7b363ab87c8712327ebb5952064eca858cd5040d
liukai234 <2679327337@qq.com>
2020-07-26 03:00:19 +0000

    ACCESS Solution26

--- algo/leetcode/Solution26.java
+++ algo/leetcode/Solution26.java
@@ -1,11 +1,39 @@
 class Solution26 {
+    // public int removeDuplicates(int[] nums) {
+
+    //     for(int i = 1; i < nums.length - count; i++) {
+    //         int quantitySameKind = 0;
+    //         int k = i;
+    //         for(;k < nums.length - count; k++) {
+    //             if(nums[k] == nums[k - 1]) quantitySameKind ++;
+    //             else break;
+    //         }
+    //         if(quantitySameKind != 0) {
+    //             count += quantitySameKind;
+    //             for(int j = i; j < nums.length - count; j ++) {
+    //                 nums[j] = nums[j + quantitySameKind];
+    //             }
+    //         }
+    //     }
+        
+
+
+    //     return nums.length - count;
+    // }
+    // public int count = 0;
     public int removeDuplicates(int[] nums) {
-        int a = 0;
-        
-        
-        
-
-
-        return a;
+        if(nums == null || nums.length == 0) return 0;
+        int p = 0;
+        int q = 1;
+        // when nums.length = 1, 1 !< nums.length; return 0 + 1 = 1;
+        while(q < nums.length) {
+            if(nums[p] == nums[q]) q ++;
+            else{
+                nums[p + 1] = nums[q]; 
+                q ++;
+                p ++;
+            } 
+        }
+        return p + 1;
     }
-}       1
+}
--- algo/leetcode/test.java
+++ algo/leetcode/test.java
@@ -5,12 +5,12 @@ public class test {
         Scanner input = new Scanner(System.in);
 
         Solution26 solution = new Solution26();
-        int[] nums = {1, 1, 2};
+        int[] nums = {1,1,1};
         int len = solution.removeDuplicates(nums);
 
         System.out.println("len: " + len);
         for (int i = 0; i < len; i++) {
-            System.out.print(nums[i]);
+            System.out.print(nums[i] + " ");
         }
         System.out.println();
         input.close();
