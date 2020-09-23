dc98a83a3b56de4941a8c31ab211faa489952fb0
liukai234 <2679327337@qq.com>
2020-09-23 04:32:16 +0000

    restruct

--- lib/Factorial.java
+++ lib/Factorial.java
@@ -6,72 +6,16 @@ public class Factorial {
 
         int a = input.nextInt();
         char[] charA = String.valueOf(a).toCharArray();
-
-        System.out.println("a->charA");
-        for(int i = 0; i < charA.length; i++) {
-            System.out.print(charA[i]);
-        }
-        System.out.println();
-        System.out.println("a->charA");
-
         input.close();
         InnerBigInteger bigInteger = null;
-        int[] result = null;
         for (int i = a - 1; i > 0; i--) {
             bigInteger = new InnerBigInteger(charA, String.valueOf(i).toCharArray());
             bigInteger.Multipy();
-            result = bigInteger.getResult();
-
-            // 类型转换 int[] result -> char[] charA
-
-            boolean flag = true; // 清除因没有进位产生的首位0
-            int length = 0;
-            length = result.length;
-            for (int index = result.length - 1; index >= 0; index--) {
-                if (result[index] != '\0')
-                    flag = false;
-
-                // boolean 使用 == 判断相等，而Boolean是boolean的封装类，Boolean作为对象必须使用equals判断相等
-                if (flag == true && result[index] == '\0')
-                    length --;
-            }
-
-            charA = new char[length];
-            for (int k = 0; k < charA.length; k++) {
-                charA[k] = (char) (result[k] + '0');
-            }
-
-            // 逆序
-            char[] tempA = new char[charA.length];
-            for(int ii = 0; ii < tempA.length; ii ++) {
-                tempA[tempA.length - 1 - ii] = charA[ii];
-            }
-            for(int ii = 0; ii < tempA.length; ii ++) {
-                charA[ii] = tempA[ii];
-            }
-
-            // for (int k = charA.length - 1; k >= 0; k--) {
-            //     System.out.print("--" + charA[k]);
-            // }
-            // System.out.println();
+            charA = bigInteger.getResult();
         }
-        // for (int i = a; i > 0; i--) {
-        // bigInteger = new InnerBigInteger(String.valueOf(a).toCharArray(),
-        // String.valueOf(i).toCharArray());
-        // bigInteger.Multipy();
-        // }
-
-        boolean flag = true; // 清除因没有进位产生的首位0
-
-        for (int i = result.length - 1; i >= 0; i--) {
-            if (result[i] != '\0')
-                flag = false;
-
-            // boolean 使用 == 判断相等，而Boolean是boolean的封装类，Boolean作为对象必须使用equals判断相等
-            if (flag == true && result[i] == '\0')
-                continue;
-
-            System.out.print(result[i]);
+        
+        for(int i = 0; i < charA.length; i ++) {
+            System.out.print(charA[i]);
         }
         System.out.println();
     }
--- lib/InnerBigInteger.java
+++ lib/InnerBigInteger.java
@@ -1,7 +1,9 @@
+// 大数相乘
 class InnerBigInteger {
     private char[] a;
     private char[] b;
     private int[] result;
+    private char[] charA;
 
     InnerBigInteger(char[] a, char[] b) {
         this.a = a;
@@ -9,12 +11,42 @@ class InnerBigInteger {
         result = new int[a.length + b.length];
     }
 
-    int[] getResult(){
-        return result;
+    // 返回char[]类型结果，高位在下标0
+    char[] getResult(){
+
+        // 类型转换 int[] result -> char[] charA
+        boolean flag = true; // 清除因没有进位产生的首位0
+
+        int length = 0;
+        length = result.length;
+        for (int index = result.length - 1; index >= 0; index--) {
+            if (result[index] != '\0')
+                flag = false;
+
+            // boolean 使用 == 判断相等，而Boolean是boolean的封装类，Boolean作为对象必须使用equals判断相等
+            if (flag == true && result[index] == '\0')
+                length --;
+        }
+
+        // 重新分配大小
+        charA = new char[length];
+        for (int k = 0; k < charA.length; k++) {
+            charA[k] = (char) (result[k] + '0');
+        }
+
+        // 逆序
+        char[] tempA = new char[charA.length];
+        for(int i = 0; i < tempA.length; i ++) {
+            tempA[tempA.length - 1 - i] = charA[i];
+        }
+        for(int i = 0; i < tempA.length; i ++) {
+            charA[i] = tempA[i];
+        }
+        return charA;
     }
 
+    // 对char[] a, char[] b执行乘法
     void Multipy() {
-        
         int[] tempa = new int[a.length];
         int[] tempb = new int[b.length];
         int p = 0;
