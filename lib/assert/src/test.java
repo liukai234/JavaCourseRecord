3dee1acecae2f572a00fdc27b35e0d96f08d9720
liukai234 <2679327337@qq.com>
2020-10-01 22:22:22 +0800

    add IDEA配置

--- lib/assert/src/test.java
+++ lib/assert/src/test.java
@@ -9,6 +9,8 @@ public class test {
         // 当assert表达式为false时从assert出停止执行并输出调试语句
         // 调试参数: -ea
         // Emp: java -ea test
+        // IDEA在项目配置VM: option中添加参数-ea
+
         assert num >= 0 : "num < 0 !";
     }
 }
