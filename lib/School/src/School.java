dcbe83c6f2111b995c8c093f553222227192022d
liukai234 <2679327337@qq.com>
2020-10-05 16:33:30 +0800

    接口中增加toString方法，类中重载实现

--- lib/School/src/School.java
+++ lib/School/src/School.java
@@ -12,6 +12,10 @@ public class School{
         class1.teacherOperation.addMember("class1's Teacher2");
         printMemOperationInfo.printMenOperationInfo(class1.teacherOperation);
 
+        System.out.println("重载sysout");
+        System.out.println(class1.teacherOperation);
+        System.out.println(class1.teacherOperation.getMember("class1's Teacher1"));
+
         class1.teacherOperation.removeMember("class1's Teacher2");
         printMemOperationInfo.printMenOperationInfo(class1.teacherOperation);
 
@@ -43,6 +47,7 @@ class Class {
 
     public class TeacherOperation implements MemberOperation {
         private LinkedList<Teacher> teachers;
+        private String stringCache;
         public TeacherOperation(LinkedList<Teacher> teachers) {
             this.teachers = teachers;
         }
@@ -97,9 +102,21 @@ class Class {
             }
             System.out.println("共有数据" + teachers.size() + "条");
         }
+
+        @Override public String toString(){
+            Iterator<Teacher> iter = teachers.iterator();
+            stringCache = "\n";
+            while(iter.hasNext()) {
+                stringCache += iter.next().getName() + '\n';
+            }
+            return stringCache;
+        }
     }
+
+
     public class StudentOperation implements MemberOperation {
         private LinkedList<Student> students;
+        private String stringCache;
         public StudentOperation(LinkedList<Student> students) {
             this.students = students;
         }
@@ -154,9 +171,19 @@ class Class {
             }
             System.out.println("共有数据" + teachers.size() + "条");
         }
+
+        @Override public String toString(){
+            Iterator<Student> iter = students.iterator();
+            stringCache = "\n";
+            while(iter.hasNext()) {
+                stringCache += iter.next().getName() + '\n';
+            }
+            return stringCache;
+        }
     }
     public class CourseOperation implements MemberOperation {
         private LinkedList<Course> courses;
+        private String stringCache;
         public CourseOperation(LinkedList<Course> courses) {
             this.courses = courses;
         }
@@ -211,9 +238,19 @@ class Class {
             }
             System.out.println("共有数据" + teachers.size() + "条");
         }
+
+        @Override public String toString(){
+            Iterator<Course> iter = courses.iterator();
+            stringCache = "\n";
+            while(iter.hasNext()) {
+                stringCache += iter.next().getName() + '\n';
+            }
+            return stringCache;
+        }
     }
     public class GrandeStudentOperation implements MemberOperation {
         private LinkedList<GrandeStudent> grandeStudents;
+        private String stringCache;
         public GrandeStudentOperation(LinkedList<GrandeStudent> grandeStudents) {
             this.grandeStudents = grandeStudents;
         }
@@ -284,6 +321,15 @@ class Class {
             }
             System.out.println("共有数据" + teachers.size() + "条");
         }
+
+        @Override public String toString(){
+            Iterator<GrandeStudent> iter = grandeStudents.iterator();
+            stringCache = "\n";
+            while(iter.hasNext()) {
+                stringCache += iter.next().getName() + '\n';
+            }
+            return stringCache;
+        }
     }
 }
 
@@ -291,9 +337,11 @@ interface Member {
     public abstract void setName(String name);
     public abstract String getName();
     public abstract void printInfo();
+    public String toString();
 }
 
 class Teacher implements Member{
+    private String stringCache;
     private String name;
     public Teacher(String name){
         this.name = name;
@@ -303,9 +351,11 @@ class Teacher implements Member{
         return name;
     }
     @Override public void printInfo() { System.out.println("Teacher Name: " + name); }
+    @Override public String toString(){ stringCache = "Teacher Name: " + name; return stringCache; }
 }
 
 class Student implements Member{
+    private String stringCache;
     private String name;
     protected int isGraduation = 0;
     protected boolean canGraduation;
@@ -315,6 +365,7 @@ class Student implements Member{
         return name;
     }
     @Override public void printInfo() { System.out.print("Student Name: " + name); }
+    @Override public String toString(){ stringCache = "Student Name: " + name; return stringCache; }
 
     public void setIsGraduation(int isGraduation) { this.isGraduation = isGraduation; }
     public int getIsGraduation() { return isGraduation; }
@@ -323,6 +374,7 @@ class Student implements Member{
 }
 
 class Course implements Member{
+    private String stringCache;
     private String name;
     public Course(String name){
         this.name = name;
@@ -332,12 +384,15 @@ class Course implements Member{
         return name;
     }
     @Override public void printInfo() { System.out.println("Course Name: " + name); }
+    @Override public String toString(){ stringCache = "Course Name: " + name; return stringCache; }
 }
 
 class GrandeStudent extends Student implements Member{
+    private String stringCache;
     private String thesis;
     public GrandeStudent(String name) { super(name); }
     @Override public void printInfo() { System.out.println("GrandeStudent Name: " + super.getName()); }
+    @Override public String toString(){ stringCache = "GrandeStudent Name: " + super.getName(); return stringCache; }
 
     public void setThesis(String thesis){ this.thesis = thesis; }
     public String getThesis(){ return thesis; }
@@ -351,6 +406,7 @@ interface MemberOperation {
     public abstract void updateMember(String originName, String newName);
     public abstract Member getMember(String name);
     public abstract void printAllMember();
+    public String toString();
 }
 
 class PrintMemOperationInfo {
