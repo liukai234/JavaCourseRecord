4abc7122877c7edbce7fef2a6112520a8584e0e1
御坂19722号 <52061371+liukai234@users.noreply.github.com>
2020-10-12 18:10:34 +0800

    将数据库处理函数封装到Connect类中

--- javaDatabase/src/Connect.java
+++ javaDatabase/src/Connect.java
@@ -1,4 +1,6 @@
 import com.mysql.cj.jdbc.Driver;
+import com.mysql.cj.result.SqlDateValueFactory;
+
 import java.util.Scanner;
 import java.sql.*;
 
@@ -15,7 +17,8 @@ public class Connect {
         this.passwd = passwd;
     }
 
-    public void connect () throws ClassNotFoundException, SQLException {
+    // 连接
+    public void connect () {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             conn = DriverManager.getConnection(uri, user, passwd);
@@ -28,5 +31,67 @@ public class Connect {
 
     public Connection getConn() {
         return conn;
+    }
+    public void closeConn() {
+        try {
+            conn.close();
+            System.out.println("Database has Disconnected");
+        } catch (SQLException e) {
+            System.out.println("e");
+        }
+    }
+    // 输出全部
+    public void printAll(String table) {
+        try {
+            String stringQuery = "SELECT * FROM " + table;
+            PreparedStatement sql = conn.prepareStatement(stringQuery);
+
+            ResultSet rs = sql.executeQuery();
+            ResultSetMetaData metaData = rs.getMetaData();
+            int columnCount = metaData.getColumnCount();
+
+            for (int i = 1; i <= columnCount; i++) {
+                System.out.print(metaData.getColumnName(i) + "    ");
+                if (i == columnCount) System.out.println();
+            }
+
+            while (rs.next()) {
+                for (int i = 1; i <= columnCount; i++) {
+                    System.out.print(rs.getString(i) + "    "); // 下标从1开始
+                    if (i == columnCount) System.out.println();
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    // 事务执行语句
+    public void commit(String ... args) {
+        try {
+            // 使用事务即一个事务中有多个sql语句但必须执行commit操作才会执行所有更改
+            conn.setAutoCommit(false); // 关闭自动提交 为true时打开自动提交
+            for (String sqlUpdateStr : args) {
+                PreparedStatement sql = conn.prepareStatement(sqlUpdateStr);
+                sql.executeUpdate();
+            }
+
+            conn.commit(); // 直到commit才会提交
+            conn.setAutoCommit(true); // 恢复自动提交
+        } catch (SQLException e) {
+            System.out.println("e");
+        }
+    }
+
+    // 普通执行语句
+    public void execute(String ... args) {
+        try {
+            for (String sqlUpdateStr : args) {
+                PreparedStatement sql = conn.prepareStatement(sqlUpdateStr);
+                sql.executeUpdate();
+            }
+        } catch (SQLException e) {
+            System.out.println("e");
+        }
     }
 }
--- javaDatabase/src/InitJDBC.java
+++ javaDatabase/src/InitJDBC.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.sql.*;
 
 public class InitJDBC {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         String uri = "jdbc:mysql://39.97.162.48:3306/school";
@@ -16,23 +16,10 @@ public class InitJDBC {
         Connect conn = new Connect(uri + uriParam, user, passwd);
         conn.connect();
 
-        try{
-            Statement sql = conn.getConn().createStatement();
-            ResultSet rs = sql.executeQuery("SELECT * FROM teachers");
-            while(rs.next()) {
-                System.out.print("ID: " + rs.getString(1)); // 下标从1开始
-                System.out.print(", LastName: " + rs.getString(2));
-                System.out.print(", FirstName: " + rs.getString(3));
-                System.out.print(", Address: " + rs.getString(4));
-                System.out.println(", City: " + rs.getString(5));
-            }
+//        conn.execute("UPDATE teachers SET LastName = 'Er' WHERE LastName = 'Qi'");
+        conn.execute("UPDATE teachers SET LastName = 'Kai' WHERE FirstName = 'Liu'");
+//        conn.execute("DELETE FROM teachers WHERE LastName = 'Er'");
 
-            conn.getConn().close();
-            System.out.println("数据库已关闭");
-        } catch (SQLException e) {
-            conn.getConn().close();
-            System.out.println("异常后数据库已关闭");
-            System.out.println(e);
-        }
+        conn.closeConn();
     }
 }
