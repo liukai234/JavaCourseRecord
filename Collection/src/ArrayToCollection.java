6d4e70533087e7bec20144474f1b5f9c623150d8
御坂19722号 <52061371+liukai234@users.noreply.github.com>
2020-10-12 20:11:17 +0800

    format line

--- Collection/src/ArrayToCollection.java
+++ Collection/src/ArrayToCollection.java
@@ -1,22 +1,19 @@
-import java.lang.reflect.Array;
 import java.util.*;
-import java.io.*;
 
 public class ArrayToCollection{
-    public static void main(String args[]) throws IOException{
-        int n = 5;         // 5 个元素
-        String[] name = new String[n];
+    public static void main(String[] args){
+
+        int n = 5;
+        String[] intString = new String[n];
         for(int i = 0; i < n; i++){
-            name[i] = String.valueOf(i);
+            intString[i] = String.valueOf(i);
         }
 
         // 将数组化为列表
-        List<String> list = Arrays.asList(name);
+        List<String> list = Arrays.asList(intString);
 
-        System.out.println();
         for(String li: list){
-            String str = li;
-            System.out.print(str + " ");
+            System.out.print(li + " ");
         }
     }
 }
