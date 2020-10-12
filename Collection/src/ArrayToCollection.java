757eaa5b952a544bd58c72a407cf35cc405a5342
御坂19722号 <52061371+liukai234@users.noreply.github.com>
2020-10-12 19:11:21 +0800

    init, add 类型互转.txt

--- Collection/src/ArrayToCollection.java
+++ Collection/src/ArrayToCollection.java
@@ -1,19 +1,23 @@
+import java.lang.reflect.Array;
 import java.util.*;
 import java.io.*;
  
 public class ArrayToCollection{
-   public static void main(String args[]) 
-   throws IOException{
+   public static void main(String args[]) throws IOException{
       int n = 5;         // 5 个元素
       String[] name = new String[n];
       for(int i = 0; i < n; i++){
          name[i] = String.valueOf(i);
       }
-      List<String> list = Arrays.asList(name); 
+
+      // 将数组化为列表
+      List<String> list = Arrays.asList(name);
+
       System.out.println();
       for(String li: list){
          String str = li;
          System.out.print(str + " ");
       }
+
    }
 }
