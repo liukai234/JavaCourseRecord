b9d0cb6ec7be6606911194f7fa676266288d5ff0
御坂19722号 <52061371+liukai234@users.noreply.github.com>
2020-10-13 21:14:21 +0800

    Init Map

--- Collection/src/MapOperation.java
+++ Collection/src/MapOperation.java
@@ -9,5 +9,12 @@ public class MapOperation {
     public void clear() {
         map.clear();
     }
+    public boolean containKey(Object key) { return map.containsKey(key); }
+    public boolean containValue(Object value) { return map.containsValue(value); }
+    public Object get(Object Key) { return map.get(Key); }
+    public boolean isEmpty() { return map.isEmpty(); }
+    public Object remove(Object key) { return map.remove(key); }
+    public int size() { return map.size(); }
+    public  void put(int key, String value) { map.put(key, value); }
 
 }
---
+++ Collection/src/MapTest.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+
+        MapOperation mapOperation = new MapOperation(map);
+
+        mapOperation.put(1, "123");
+        System.out.println(mapOperation.size());
+        System.out.println(mapOperation.get(1));
+
+    }
+}
